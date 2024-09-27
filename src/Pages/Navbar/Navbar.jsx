1092119e9ef2bd374b434cd2bbaf91636a2a06f5
safi islam <118913244+safiislam@users.noreply.github.com>
2024-09-27 06:35:52 +0600

    add

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Navbar = () => {
-    return (
-        <nav className="bg-gray-800 text-white p-4 fixed top-0 w-full z-10">
+  return (
+    <nav className="bg-gray-800 text-white p-4 fixed top-0 w-full z-10">
       <div className="container mx-auto">Manson Market</div>
     </nav>
-    );
+  );
 };
 
 export default Navbar;
